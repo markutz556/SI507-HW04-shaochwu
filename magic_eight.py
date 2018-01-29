9fb63784300e965f750b3bdad375762013ef99af
githubxinchunli <31594269+githubxinchunli@users.noreply.github.com>
2018-01-29 16:52:32 -0500

    add_questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 question = str(input("What is your question?"))
+
+import random
+answer_list = ["It is certain","It is decidedly so", "Without a doubt",
+				"Yes definitely, You may rely on it", "As I see it, yes",
+				"Most likely", "Outlook good", "Yes", "Signs point to yes",
+				"Reply hazy try again", "Ask again later", "Better not tell you now",
+				"Cannot predict now", "Concentrate and ask again", "Don't count on it",
+				"My reply is no", "My sources say no", "Outlook not so good",
+				"Very doubtful"]
+random_number = randrange(0, 20)
+answer = answer_list([random_number])
