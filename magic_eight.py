cd37879b595e376a2243e478e7a6966d289af9ee
markutz556 <2917097440@qq.com>
2018-01-29 17:00:24 -0500

    add check_question

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 question = str(input("What is your question?"))
+while again:
+	if question.endswith("?"):
+		print(answers_list[answers])
+	elif question == "quit":
+		again = False
+	else:
+		print("I'm sorry, I can only answer questions.")
